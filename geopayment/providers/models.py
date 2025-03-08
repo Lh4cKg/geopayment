4dfb60731f1985a3e4acc7aa1f7a82926f0eb623
Lasha Gogua <lh4ckg@gmail.com>
2025-03-09 02:25:11 +0400

    fix update BaseModel and validation

--- geopayment/providers/models.py
+++ geopayment/providers/models.py
@@ -1,13 +1,16 @@
 import re
 import types
 import typing as t
+import uuid
+from datetime import datetime
 from dataclasses import dataclass, asdict, is_dataclass, MISSING, Field
 from decimal import Decimal, ROUND_UP
 
 try:
     import ujson
+    dumps = ujson.dumps
 except ImportError:
-    pass
+    from json import dumps
 
 from geopayment.utils.common import (
     serialize_dict_factory,
@@ -18,26 +21,26 @@ from geopayment.utils.common import (
 @dataclass
 class BaseModel:
 
-    __config__: t.ClassVar[t.Dict[str, t.Any]] = {
+    __config__: t.ClassVar[dict[str, bool]] = {
         'validation': False
     }
 
-    def to_dict(self, dropna: bool = False) -> t.Dict[t.Any, t.Any]:
+    def to_dict(self, dropna: bool = False) -> dict[t.Any, t.Any]:
         if dropna:
             return asdict(self, dict_factory=dropna_serialize_dict_factory)
         return asdict(self, dict_factory=serialize_dict_factory)
 
     def to_json(self, dropna: bool = False) -> str:
         if dropna:
-            return ujson.dumps(self.to_dict(dropna=dropna))
-        return ujson.dumps(self.to_dict())
+            return dumps(self.to_dict(dropna=dropna))
+        return dumps(self.to_dict())
 
     def __post_init__(self):
         if self.__config__['validation']:
             cls = type(self)
             annotations = t.get_type_hints(cls)
             for field, type_hint in annotations.items():
-                if field.startswith('__') and field.endswith('__'):
+                if field == '__config__':
                     continue
 
                 dc_field = self.__dataclass_fields__[field]
@@ -55,20 +58,43 @@ class BaseModel:
                     )
                 elif isinstance(type_hint, types.UnionType):
                     for union_arg in t.get_args(type_hint):
-                        if isinstance(union_arg, (types.GenericAlias, t._GenericAlias)):
+                        is_dc = is_dataclass(union_arg)
+                        if is_dc:
+                            if not isinstance(value, union_arg):
+                                self.__setattr__(field, union_arg(**value))
+                            break
+                        elif isinstance(union_arg, (types.GenericAlias, t._GenericAlias)):
                             self.__generic_alias_validation(
                                 field=field, value=value, type_hint=union_arg
                             )
+                            break
+                        elif union_arg is Decimal:
+                            self.__decimal_validation(field, value)
+                            break
+                        elif union_arg is uuid.UUID:
+                            self.__setattr__(field, uuid.UUID(value))
+                            break
                 elif isinstance(value, type_hint):
                     continue
+                elif is_dataclass(type_hint) and isinstance(value, dict):
+                    self.__setattr__(field, type_hint(**value))
                 elif type_hint is Decimal:
-                    self.__decimal_validation(field, value, type_hint)
+                    self.__decimal_validation(field, value)
                 elif type_hint is int:
-                    self.__int_validation(field, value, type_hint)
+                    self.__int_validation(field, value)
                 elif type_hint is float:
-                    self.__float_validation(field, value, type_hint)
+                    self.__float_validation(field, value)
                 elif type_hint is str:
                     self.__setattr__(field, str(value))
+                elif type_hint is uuid.UUID:
+                    self.__setattr__(field, uuid.UUID(value))
+                elif (
+                        isinstance(type_hint, t._LiteralGenericAlias)
+                        and self.__literal_validation(value, type_hint)
+                ):
+                    self.__setattr__(field, value)
+                elif type_hint is datetime:
+                    self.__datetime_validation(field, value)
 
     # Helper functions
 
@@ -112,6 +138,17 @@ class BaseModel:
     @staticmethod
     def __numeric_validation(value: t.Any) -> bool:
         return bool(re.match(r'(\d+\.\d+|\d+)', value))
+
+    def __datetime_validation(self, field, value: str) -> bool:
+        try:
+            if 'T' in value and 'Z' in value:
+                dt = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%fZ')
+            else:
+                dt = datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
+            self.__setattr__(field, dt)
+        except ValueError:
+            return False
+        return True
 
     @staticmethod
     def __literal_validation(value: t.Any, type_hint: t.Generic) -> t.Literal[True]:
@@ -158,6 +195,15 @@ class BaseModel:
                                 values.append(union_arg(**i))
                             self.__setattr__(field, values)
                             break
+                elif isinstance(arg, types.UnionType):
+                    for union_arg in t.get_args(arg):
+                        is_dc = is_dataclass(union_arg)
+                        if is_dc:
+                            if isinstance(value, dict):
+                                self.__setattr__(field, union_arg(**value))
+                            elif isinstance(value, list):
+                                value = [union_arg(**i) for i in value]
+                                self.__setattr__(field, value)
         elif origin is t.Union:
             for arg in t.get_args(type_hint):
                 if isinstance(arg, t._LiteralGenericAlias):
@@ -182,6 +228,6 @@ class BaseModel:
 @dataclass
 class ValidationModel(BaseModel):
 
-    __config__: t.ClassVar[t.Dict[str, t.Any]] = {
+    __config__: t.ClassVar[dict[str, bool]] = {
         'validation': True
     }
